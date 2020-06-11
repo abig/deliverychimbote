ae4e192267a4cbbc684c890a1ff2032053df775d
Diego Albitres <diego.albitres@gmail.com>
2020-06-11 01:30:49 -0500

    Removed all references to ListItem

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,15 +1,12 @@
 import { useContext, useState } from 'react'
 import Promise from 'promise-polyfill'
-import Obfuscate from 'react-obfuscate'
 import { LanguageContext } from '../../components/LanguageSelector'
 import Head from '../../components/Head'
 import Nav from '../../components/Nav'
 import Footer from '../../components/Footer'
 import LoadingSpinner from '../../components/LoadingSpinner'
-import WhatsAppLogo from '../../components/WhatsAppLogo'
 import { FiltersList, FiltersTranslation } from '../../components/FiltersList'
-import { OutboundLink } from 'react-ga'
-import { Event } from '../../components/Analytics'
+import ListItem from '../../components/ListItem'
 
 const pageContent = {
   'es-PE': {
@@ -21,12 +18,8 @@ const pageContent = {
     businessTypes: FiltersTranslation["es-PE"].businessTypes,
     selectType: 'Seleccione un rubro...',
     zonesLabel: 'Zonas',
-    moreZones: 'Ver zonas donde llegamos',
     offersLabel: 'Productos',
     offers: FiltersTranslation["es-PE"].offers,
-    delivery: 'Delivery disponible',
-    whatsappLabel: 'Pedir por WhatsApp',
-    orderLabel: 'Ir a web',
   },
   'en-US': {
     title: 'Businesses',
@@ -37,152 +30,9 @@ const pageContent = {
     businessTypes: FiltersTranslation["en-US"].businessTypes,
     selectType: 'Select a business type...',
     zonesLabel: 'Zones',
-    moreZones: 'See zones were we get',
     offersLabel: 'Offers',
     offers: FiltersTranslation["en-US"].offers,
-    delivery: 'Delivery available',
-    whatsappLabel: 'Order via WhatsApp',
-    orderLabel: 'Website',
   },
-}
-
-const ListItem = ({ restaurant, content }) => {
-  const name = restaurant.name || undefined
-  const address = restaurant.address || undefined
-  const description = restaurant.description || undefined
-  const district = restaurant.district || undefined
-  const zones = restaurant.zones || undefined
-  const offers = restaurant.offerings || undefined
-  const delivery = restaurant.delivery || false
-  const phone = restaurant.phone
-    ? restaurant.phone.includes("+51")
-      ? restaurant.phone
-      : "+51" + restaurant.phone
-    : undefined
-  const url = restaurant.url || undefined
-  const whatsapp = restaurant.whatsapp || undefined
-  const email = restaurant.email
-    ? restaurant.email.toLowerCase()
-    : undefined
-  const addrQuery = restaurant.pluscode
-    ? encodeURIComponent(restaurant.pluscode)
-    : encodeURIComponent(restaurant.address)
-
-  const [zoneCollapse, setZoneCollapse] = useState(false)
-
-  return (
-    <li className="w-full md:w-1/2 p-3">
-      <div className="rounded relative h-full flex flex-col items-start border border-sand overflow-hidden p-4 sm:p-8 lg:px-12">
-        <div className="flex-auto">
-          {name && <h3 className="uppercase text-xl sm:text-2xl">{name}</h3>}
-          {district && district.length && <p className="text-xs sm:text-sm mb-4">{district.join(", ")}</p>}
-          {address && 
-            <p className="text-sm mb-2">
-              <OutboundLink
-                eventLabel={`https://www.google.com/maps/place/?q=${addrQuery}`}
-                to={`https://www.google.com/maps/place/?q=${addrQuery}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {address}
-              </OutboundLink>
-            </p>
-          }
-          {email &&
-            <p className="text-sm mb-2">
-              <button onClick={() => Event("Business Email", "Click", name)}>
-                <Obfuscate
-                  className="underline"
-                  email={email}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                />
-              </button>
-            </p>
-          }
-          {phone && 
-            <p className="text-sm mb-4">
-              <button onClick={() => Event("Business Phone", "Click", name)}>
-                <Obfuscate
-                  className="underline"
-                  tel={phone}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                />
-              </button>
-            </p>
-          }
-          {description && (
-            <p className="max-w-xl text-sm sm:text-base mb-4">{description}</p>
-          )}
-          {offers && !!offers.length && (
-            <ul className="-m-1 mb-6">
-              {offers.map(offer => (
-                <li
-                  key={offer}
-                  className="inline-block rounded font-medium text-xs sm:text-sm bg-sand px-2 py-1 m-1"
-                >
-                  {offer}
-                </li>
-              ))}
-            </ul>
-          )}
-          {zones && !!zones.length &&
-            <button 
-              className="underline text-xs sm:text-sm mb-4"
-              onClick={() => setZoneCollapse(!zoneCollapse)}
-            >+ {content.moreZones}</button>
-          }
-          {zoneCollapse &&
-            <ul className="-m-1 mb-6">
-              {zones.map(zone => (
-                <li
-                  key={zone}
-                  className="inline-block rounded font-medium text-xs sm:text-sm bg-sand px-2 py-1 m-1"
-                >
-                  {zone}
-                </li>
-              ))}
-            </ul>
-          }
-        </div>
-        <div className="mt-4 items-center">
-        {phone && whatsapp && (
-            <button onClick={() => Event("WhatsApp", "Click", name)}>
-              <Obfuscate
-                href={`https://api.whatsapp.com/send?phone=${phone}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-primary h-full mr-4 rounded text-sm"
-                obfuscateChildren={false}
-              >
-                {content.whatsappLabel}&nbsp;&nbsp;&nbsp;
-                <WhatsAppLogo className="inline flex-auto text-right" />
-              </Obfuscate>
-            </button>
-          )}
-          {url && (
-            <button className="mt-2">
-              <OutboundLink
-                eventLabel={name}
-                to={url.includes('http') ? url : 'https://' + url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-secondary h-full rounded text-sm"
-              >
-                {content.orderLabel}&nbsp;&nbsp;&nbsp;⟶
-              </OutboundLink>
-            </button>
-          )}
-        </div>
-        {delivery && (
-          <div className="sm:absolute rounded top-0 right-0 font-medium text-sm sm:bg-sand sm:border-b border-sand sm:px-2 sm:py-1 mt-4 sm:m-2">
-            ✓ {content.delivery}
-          </div>
-        )}
-      </div>
-    </li>
-  )
 }
 
 const FilterLabel = ({ handleChange, isChecked, label }) => (
@@ -202,7 +52,7 @@ const FilterLabel = ({ handleChange, isChecked, label }) => (
   </label>
 )
 
-export default ({ restaurants }) => {
+export default ({ items }) => {
   const { language } = useContext(LanguageContext)
   const content = pageContent[language]
 
@@ -212,7 +62,7 @@ export default ({ restaurants }) => {
   const [filterOffers, setFilterOffers] = useState([])
   const [filterDelivery, setFilterDelivery] = useState(false)
 
-  if (restaurants)
+  if (items)
     return (
       <>
         <Head />
@@ -358,55 +208,54 @@ export default ({ restaurants }) => {
                 />
               </label>
               <ul className="flex flex-wrap -m-3">
-                {restaurants
+                {items
                   // Filter for necessary content
                   .filter(
-                    restaurant =>
-                      restaurant.display &&
-                      restaurant.name &&
-                      restaurant.description &&
-                      restaurant.district &&
-                      restaurant.address 
+                    item =>
+                      item.display &&
+                      item.name &&
+                      item.description &&
+                      item.district &&
+                      item.address 
                   )
                   // Filter for district
-                  .filter(restaurant =>
+                  .filter(item =>
                     filterDistrict
-                      ? restaurant.district.includes(filterDistrict)
+                      ? item.district.includes(filterDistrict)
                       : true  
                   )
                   // Filter for zones
-                  .filter(restaurant =>
+                  .filter(item =>
                     filterZone && filterZone.length
                       ? filterZone.some(zone =>
-                          restaurant.zones.includes(filterDistrict + ": " + zone)
+                          item.zones.includes(filterDistrict + ": " + zone)
                         )
                       : true
                   )
                   // Filter for business type
-                  .filter(restaurant =>
+                  .filter(item =>
                     filterTypes && filterTypes.length
                       ? filterTypes.some(type =>
-                          restaurant.businesstype.includes(type)
+                          item.businesstype.includes(type)
                         )
                       : true
                   )
                   // Filter for offers
-                  .filter(restaurant =>
+                  .filter(item =>
                     filterOffers && filterOffers.length
                       ? filterOffers.some(offer =>
-                          restaurant.offerings.includes(offer)
+                          item.offerings.includes(offer)
                         )
                       : true
                   )
                   // Filter for delivery
-                  .filter(restaurant =>
-                    filterDelivery ? restaurant.delivery : true
+                  .filter(item =>
+                    filterDelivery ? item.delivery : true
                   )
-                  .map((restaurant, index) => (
+                  .map((item, index) => (
                     <ListItem
                       key={index}
-                      restaurant={restaurant}
-                      content={content}
+                      item={item}
                     />
                   ))}
               </ul>
@@ -431,16 +280,16 @@ export async function getStaticProps() {
   const airtable = new Airtable({
     apiKey: airtableApiKey,
   }).base(airtableBaseKey)
-  const base = await airtable('Restaurants')
+  const base = await airtable('Negocios')
   const records = await base
     .select({
       maxRecords: 999999, // don't want to paginate...
       view: 'Grid view', // NOTE: changing the view name will break things
     })
     .all()
-  const restaurants = await Promise.all(
+  const items = await Promise.all(
     records.sort(() => 0.5 - Math.random()).map(record => record.fields)
   )
 
-  return { props: { restaurants } }
+  return { props: { items } }
 }
