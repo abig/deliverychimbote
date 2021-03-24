4073e4165c9f9ff8c877dfd384b50016b0448e14
Diego Albitres <diego.albitres@gmail.com>
2021-03-24 15:13:07 -0500

    Updated analytics to use id from env

--- components/Analytics.js
+++ components/Analytics.js
@@ -1,7 +1,9 @@
 import ReactGA from 'react-ga'
 
+const TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+
 export const initGA = () => {
-  ReactGA.initialize("UA-54078107-7")
+  ReactGA.initialize(TRACKING_ID)
 }
 
 export const logPageView = () => {
