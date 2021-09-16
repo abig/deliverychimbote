d7490a162c56b9ba43485cb17bcf2753e149b035
Diego Albitres <diego.albitres@gmail.com>
2021-09-16 14:42:04 -0500

    Fixed Geocoding API access

--- pages/map.js
+++ pages/map.js
@@ -5,8 +5,10 @@ import Nav from '../components/Nav';
 import Map from '../components/Map';
 import slugify from 'slugify';
 import { NextSeo } from 'next-seo';
+import Airtable from 'airtable';
+import { Client as MapsClient } from '@googlemaps/google-maps-services-js';
 
-// start search from google maps: https://www.google.com/maps/place/?q=something (url encoded)
+// Start search from google maps: https://www.google.com/maps/place/?q=something (url encoded)
 
 const pageContent = {
   'es-PE': {
@@ -63,18 +65,14 @@ export default MapPage;
 export async function getStaticProps() {
   const airtableApiKey = process.env.AIRTABLE_API_KEY;
   const airtableBaseKey = process.env.AIRTABLE_BASE_KEY;
-  // Reducing number of requests to Maps API
-  const googleMapsApiKey =
-    process.env.NODE_ENV === 'production'
-      ? process.env.GOOGLE_MAPS_API_KEY
-      : undefined;
-
-  const Airtable = require('airtable');
+  const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
 
   const airtable = new Airtable({
     apiKey: airtableApiKey,
   }).base(airtableBaseKey);
   const base = airtable('Negocios');
+
+  const maps = new MapsClient();
   
   const records = await base
     .select({
@@ -90,56 +88,39 @@ export async function getStaticProps() {
 
   const recordsToUpdate = [];
 
-  const items = await Promise.all(records.map(async ({ id, fields }) => {
-    const item = fields;
+  const items = await Promise.all(records.map(async item => {
+    const fields = item.fields;
 
-    if (!item["Geocode JSON"]) {
-      const positionData = await fetch(getMapsUrl(item, googleMapsApiKey))
-        .then(res => JSON.parse(res))
-        .catch(err => console.log(err));
-
-      if (positionData) {
-        item["Geocode JSON"] = positionData;
-
-        recordsToUpdate.push({
-          id,
-          fields: {
-            ["Geocode JSON"]: JSON.stringify(positionData)
-          }
-        });
-      }
-    } else {
-      item["Geocode JSON"] = JSON.parse(item["Geocode JSON"]);
+    if (fields["Geocode JSON"]) {
+      fields["Geocode JSON"] = JSON.parse(fields["Geocode JSON"]);
+      return fields;
     }
 
-    return item;
+    const positionData = await maps.geocode({
+      params: {
+        address: fields["Código Plus"] || `${fields["Dirección"]} ${fields["Distritos"][0]}`,
+        key: googleMapsApiKey,
+      },
+    });
+
+    if (positionData.data) {
+      fields["Geocode JSON"] = positionData.data;
+
+      recordsToUpdate.push({
+        id: item.id,
+        fields: {
+          ["Geocode JSON"]: JSON.stringify(positionData.data)
+        }
+      });
+    }
+
+    return fields;
   }));
 
   for (let i = 0; i < recordsToUpdate.length; i += 10) {
     await base.update(recordsToUpdate.slice(i, i + 10))
-      .catch(err => console.log(err));
+      .catch(err => console.error(err));
   }
 
   return { props: { items } };
 }
-
-const getMapsUrl = (record, mapsApiKey) => {
-  if (!record || !mapsApiKey)
-    return "";
-
-  const district = Array.isArray(record["Distritos"])
-    ? record["Distritos"][0]
-    : record["Distritos"];
-
-  const query = Object.prototype.hasOwnProperty.call(record, "Código Plus")
-    ? record["Código Plus"]
-    : record["Dirección"] + " " + district;
-
-  const url =
-    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
-    encodeURIComponent(query) +
-    "&key=" +
-    mapsApiKey;
-
-  return url;
-};
