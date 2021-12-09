d28144ea655111f346c55191328dd33bdea23327
Diego Albitres <diego.albitres@gmail.com>
2021-12-09 00:21:43 -0500

    Started migration to GA4

--- components/Analytics.js
+++ components/Analytics.js
@@ -1,9 +1,10 @@
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
-const TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+const MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export const initGA = () => {
-  ReactGA.initialize(TRACKING_ID);
+  ReactGA.initialize(MEASUREMENT_ID);
 };
 
 export const logPageView = () => {
